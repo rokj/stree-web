86e17d9bd09ee41abfa5a3502b3d034e2eae0ab7
Rok Jaklič <rok.jaklic@arnes.si>
2024-04-22 16:41:20 +0200

    missing file

--- js/settings.dist.js
+++ js/settings.dist.js
@@ -1,20 +1,23 @@
-export let app_endpoint = "https://app/";
-export let s3_endpoint = "https://s3";
+import * as settings from "./settings";
+
+export let s3_endpoint = "https://s3_endpoint";
 export let bucket_name = "storage";
+export let app_endpoint = "https://app_endpoint/";
+
 export let debug = true;
 export let stree_version_key = "stree_version";
 export let urls = {
-        get_credentials: "/get-ec/",
         get_user_shares: "/get-user-shares/",
         share: "/share/",
         unshare: "/unshare/",
+        download_shared_object: `${settings.app_endpoint}download/?k=`,
+        get_credentials: "/get-ec/",
         share_document: "/share-document/"
 };
 
 export let language = "en";
 
 /*
- todo
- e.g: export let plugins = ['sharing', 'onlyoffice'];
+  e.g: export let plugins = ['sharing', 'update_versions', 'onlyoffice'];
  */
 export let plugins = [];
